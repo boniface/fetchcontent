43a26785dcb6543df59999f63a0617dec9faa977
hashcode <boniface@kabaso.com>
2021-07-28 21:46:28 +0200

    Update

---
+++ pre-tasts.md
@@ -0,0 +1,9 @@
+## tasks 
+```bash
+
+rustup component add clippy
+rustup component add rustfmt
+cargo install cargo-tarpaulin
+cargo install cargo-audit
+cargo install cargo-edit 
+```
--- src/configuration.rs
+++ src/configuration.rs
@@ -1,3 +1,9 @@
+use std::sync::Arc;
+
+use cdrs_tokio::cluster::{ClusterTcpConfig, NodeTcpConfigBuilder};
+use cdrs_tokio::load_balancing::RoundRobin;
+use cdrs_tokio::retry::DefaultRetryPolicy;
+
 #[derive(serde::Deserialize)]
 pub struct Settings {
     pub database: DatabaseSettings,
@@ -26,6 +32,33 @@ impl DatabaseSettings {
             "postgres://{}:{}@{}:{}",
             self.username, self.password, self.host, self.port
         )
+    }
+}
+
+#[derive(serde::Deserialize)]
+pub struct CassandraSettings {
+    pub username: String,
+    pub password: String,
+    pub port: u16,
+    pub host: String,
+    pub keyspace_name: Str,
+}
+
+impl CassandraSettings {
+    pub fn getCassandraSession(&self) -> Arc<CurrentSession> {
+        let node =
+            NodeTcpConfigBuilder::new(
+                "127.0.0.1:9042", Arc::new(
+                    NoneAuthenticatorProvider)
+            ).build();
+        let cluster_config = ClusterTcpConfig(vec![node]);
+        let lb = RoundRobin::new();
+        let session = Arc::new(
+            new_session(&cluster_config, lb, Box::new(DefaultRetryPolicy::default()))
+                .await
+                .expect("session should be created"),
+        );
+        session
     }
 }
 
